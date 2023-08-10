b47716c1ecda1d37a6e54807a8d3590c40e46936
Kamrang <72611915+Kamrang-aja@users.noreply.github.com>
2023-08-10 20:59:25 +0700

    Update backup_distlist_numbers.sh.sh

--- archives/export/distlist/distlist_numbers/backup_distlist_numbers.sh.sh
+++ archives/export/distlist/distlist_numbers/backup_distlist_numbers.sh.sh
@@ -1,5 +1,5 @@
 #=== Scrip get all members in distribution lists in mail server =========================
-for i in `cat /opt/zimbra/backup/archives/export/distlist/distlist.txt`; 
-	do zmprov gdlm $i > /opt/zimbra/backup/archives/export/distlist/distlist_numbers/$i.txt ;
+for i in `cat /archives/export/distlist/distlist.txt`; 
+	do zmprov gdlm $i > /archives/export/distlist/distlist_numbers/$i.txt ;
 	echo "$i"; 
 done
