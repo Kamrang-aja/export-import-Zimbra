3695a9fb1c6ebcc0952d5a9b4dab8f92ab669769
Kamrang <72611915+Kamrang-aja@users.noreply.github.com>
2023-08-10 21:04:12 +0700

    Update and rename backup_signatures.sh.sh to backup_signatures.sh

--- archives/export/signatures/backup_signatures.sh
+++ archives/export/signatures/backup_signatures.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #===== Scrip create signature for emails =========================
-for i in `cat /opt/zimbra/backup/archives/export/emails/emails.txt`; do
+for i in `cat /archives/export/emails/emails.txt`; do
     zmprov ga $i zimbraPrefMailSignatureHTML > /tmp/signature;
     sed -i -e "1d" /tmp/signature ;
-    sed 's/zimbraPrefMailSignatureHTML: //g' /tmp/signature > /opt/zimbra/backup/archives/export/signatures/$i.signature ;
+    sed 's/zimbraPrefMailSignatureHTML: //g' /tmp/signature > /archives/export/signatures/$i.signature ;
     rm -rf /tmp/signature;
     `zmprov ga $i zimbraSignatureName > /tmp/name` ;
     sed -i -e "1d" /tmp/name ;
-    sed 's/zimbraSignatureName: //g' /tmp/name > /opt/zimbra/backup/archives/export/signatures/$i.name ;
+    sed 's/zimbraSignatureName: //g' /tmp/name > /archives/export/signatures/$i.name ;
     rm -rf /tmp/name ;
 done
