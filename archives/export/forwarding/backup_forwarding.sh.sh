c74ff37ab51ed935fceb687ef820aa580b274ab4
Kamrang <72611915+Kamrang-aja@users.noreply.github.com>
2023-08-10 21:00:40 +0700

    Update backup_forwarding.sh.sh

--- archives/export/forwarding/backup_forwarding.sh.sh
+++ archives/export/forwarding/backup_forwarding.sh.sh
@@ -1,7 +1,7 @@
 #=== Scrip find all mail forwarding in mail server =========================
-for i in `cat /opt/zimbra/backup/archives/export/emails/emails.txt`; 
-	do zmprov  -l ga $i zimbraPrefMailForwardingAddress | grep zimbraPrefMailForwardingAddress: | awk '{ print $2}' > /opt/zimbra/backup/archives/export/forwarding/$i.forward; 
+for i in `cat /archives/export/emails/emails.txt`; 
+	do zmprov  -l ga $i zimbraPrefMailForwardingAddress | grep zimbraPrefMailForwardingAddress: | awk '{ print $2}' > /archives/export/forwarding/$i.forward; 
 done 
 
 #=== Scrip remove those empty files as follows =========================
-find /opt/zimbra/backup/archives/export/forwarding/ -type f -empty | xargs -n1 rm -v
+find /archives/export/forwarding/ -type f -empty | xargs -n1 rm -v
