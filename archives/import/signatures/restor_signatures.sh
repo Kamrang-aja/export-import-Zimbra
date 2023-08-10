b84aa0367871a9a58f65ed74b9fac8daf986a00b
Kamrang <72611915+Kamrang-aja@users.noreply.github.com>
2023-08-10 21:08:14 +0700

    Update restor_signatures.sh

--- archives/import/signatures/restor_signatures.sh
+++ archives/import/signatures/restor_signatures.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #=== Scrip restort signatures in mail server =========================
-for i in `cat /opt/zimbra/backup/archives/import/emails.txt`; do
-    zmprov ma $i zimbraSignatureName "`cat /opt/zimbra/backup/archives/import/signatures/$i.name`";
-    zmprov ma $i zimbraPrefMailSignatureHTML "`cat /opt/zimbra/backup/archives/import/signatures/$i.signature`";
+for i in `cat /archives/import/emails.txt`; do
+    zmprov ma $i zimbraSignatureName "`cat /archives/import/signatures/$i.name`";
+    zmprov ma $i zimbraPrefMailSignatureHTML "`cat /archives/import/signatures/$i.signature`";
     zmprov ga $i zimbraSignatureId > /tmp/firmaid; sed -i -e "1d" /tmp/firmaid;
     firmaid=`sed 's/zimbraSignatureId: //g' /tmp/firmaid`;
     zmprov ma $i zimbraPrefDefaultSignatureId "$firmaid";
