8a10b3aa247d02205e376e399d1f737a6c0ddacc
Kamrang <72611915+Kamrang-aja@users.noreply.github.com>
2023-08-10 20:58:48 +0700

    Update backup_alias.sh

--- archives/export/alias/backup_alias.sh
+++ archives/export/alias/backup_alias.sh
@@ -1,8 +1,8 @@
 #=== Scrip backup alias in mail server =========================
 for i in `cat /opt/zimbra/backup/archives/export/emails/emails.txt`; 
-	do zmprov ga  $i | grep zimbraMailAlias |awk '{print $2}' > /opt/zimbra/backup/archives/export/alias/$i.txt ;
+	do zmprov ga  $i | grep zimbraMailAlias |awk '{print $2}' > /archives/export/alias/$i.txt ;
 	echo $i ;
 done
 
 #=== Scrip remove those empty files as follows =========================
-find /opt/zimbra/backup/archives/export/alias/ -type f -empty | xargs -n1 rm -v 
+find /archives/export/alias/ -type f -empty | xargs -n1 rm -v 
