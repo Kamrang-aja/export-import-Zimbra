e73e174a92716c67d879f425abf1b9a2d8fc6df4
Kamrang <72611915+Kamrang-aja@users.noreply.github.com>
2023-08-10 21:07:28 +0700

    Update restor_filters.sh

--- archives/import/filters/restor_filters.sh
+++ archives/import/filters/restor_filters.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #=== Scrip restort filters in mail server =========================
-for i in `cat /opt/zimbra/backup/archives/import/emails.txt`; do
-    zmprov ma  $i zimbraMailSieveScript "`cat /opt/zimbra/backup/archives/import/filter/$i.filter`";
+for i in `cat /archives/import/emails.txt`; do
+    zmprov ma  $i zimbraMailSieveScript "`cat /archives/import/filter/$i.filter`";
     echo "Filter Restore for account ... $i"; 
 
 done
