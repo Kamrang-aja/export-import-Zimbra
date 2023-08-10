0e905f20d5c9731f83d40e9e8853904cf046c06f
Kamrang <72611915+Kamrang-aja@users.noreply.github.com>
2023-08-10 21:05:29 +0700

    Update restor_CoS.sh

--- archives/import/CoA/restor_CoS.sh
+++ archives/import/CoA/restor_CoS.sh
@@ -3,9 +3,9 @@
 # user to the COS. 
 
 #=== Scrip restort CoS in mail server =========================
-for cos in `/bin/ls -1 /opt/zimbra/backup/archives/import/cos_users | tr " " "\n"`
+for cos in `/bin/ls -1 /archives/import/cos_users | tr " " "\n"`
 do
-	for i in `cat /opt/zimbra/backup/archives/import/cos_users/$cos`
+	for i in `cat /archives/import/cos_users/$cos`
 		do
 		zmprov sac $i $cos
 		done
