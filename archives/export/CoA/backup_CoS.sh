fc31434c5538b169e9ca44cdbd507b2374f83a91
Kamrang <72611915+Kamrang-aja@users.noreply.github.com>
2023-08-10 20:56:29 +0700

    Update backup_CoS.sh

--- archives/export/CoA/backup_CoS.sh
+++ archives/export/CoA/backup_CoS.sh
@@ -7,16 +7,15 @@
 # the end. Zimbra only detect users that are not in the default COS. Thus, every non detected user is in the default COS. 
 
 #=== Scrip backup all CoS in mail server =========================
-mkdir /tmp/cos_users
-IFS=$'\n'
 source ~/bin/zmshutil; zmsetvars
 
 for i in `zmprov gac`
    do
-       id=$(zmprov gc "$i" zimbraId | grep zimbraId | cut -f2 -d":" | tr -d ' ')
+       id=`zmprov gc $i | grep -e "\bzimbraId\b"|cut -d " " -f 2`
 
-       /opt/zimbra/common/bin/ldapsearch -xLLL -o ldif-wrap=no -h `zmhostname`:389 -D $zimbra_ldap_userdn -w $zimbra_ldap_password "(&(objectclass=zimbraAccount)(zimbraCosId="$id"))" mail > "/opt/zimbra/backup/archives/export/cos_users/$i"
+       ldapsearch -x -H $ldap_master_url -D $zimbra_ldap_userdn -w $zimbra_ldap_password| grep -e $id -e "dn: uid=" -e mail:|grep -v Email| grep -e zimbraCOSId -B1 -A1|grep mail|awk '{print $2}' > /opt/zimbra/backup/archives/export/cos_users/$i
 
 /bin/rm -rf /opt/zimbra/backup/archives/export/cos_users/default*
 
 done
+
