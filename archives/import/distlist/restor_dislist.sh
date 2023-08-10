4a44f1ad8355989ea704dc24cbffa29a4b4e8f81
Kamrang <72611915+Kamrang-aja@users.noreply.github.com>
2023-08-10 21:06:30 +0700

    Update restor_dislist.sh

--- archives/import/distlist/restor_dislist.sh
+++ archives/import/distlist/restor_dislist.sh
@@ -1,5 +1,5 @@
 #=== Scrip restor distribution lists (By Kamrang) =========================
-for i in `cat /opt/zimbra/backup/archives/import/distributinlist.txt`; 
+for i in `cat /archives/import/distributinlist.txt`; 
 	do zmprov cdl $i ; 
 	echo "$i -- done " ; 
 done
