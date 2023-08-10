4a390161aed6f735a495e1305b6a263f3d4e1d92
Kamrang <72611915+Kamrang-aja@users.noreply.github.com>
2023-08-10 21:05:45 +0700

    Update restor_alias.sh

--- archives/import/alias/restor_alias.sh
+++ archives/import/alias/restor_alias.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #=== Scrip restor distribution lists =========================
-for i in `cat /opt/zimbra/backup/archives/importemails.txt`
+for i in `cat /archives/importemails.txt`
 do
     if [ -f "/opt/zimbra/backup/archives/import/alias/$i.txt" ]; then
-    for j in `grep '@' /opt/zimbra/backup/archives/import/alias/$i.txt`
+    for j in `grep '@' /archives/import/alias/$i.txt`
     do
     zmprov aaa $i $j
     echo "$i HAS ALIAS $j --- Restored"
