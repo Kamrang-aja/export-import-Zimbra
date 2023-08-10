2b2ebc4be9e7ce995b399f1756ef3f433bdbf8b6
Kamrang <72611915+Kamrang-aja@users.noreply.github.com>
2023-08-10 21:06:17 +0700

    Update restor_dislist_members.sh

--- archives/import/distlist/distlist_members/restor_dislist_members.sh
+++ archives/import/distlist/distlist_members/restor_dislist_members.sh
@@ -2,9 +2,9 @@
 # add all memebers to each of these distribution lists
 
 #=== Scrip restort distribution members in mail server =========================
-for i in `cat /opt/zimbra/backup/archives/import/distributinlist.txt`
+for i in `cat /archives/import/distributinlist.txt`
 do
-    for j in `grep -v '#' /opt/zimbra/backup/archives/import/distributinlist_members/$i.txt |grep '@'` 
+    for j in `grep -v '#' /archives/import/distributinlist_members/$i.txt |grep '@'` 
     do
     zmprov adlm $i $j
     echo " $j member has been added to list $i"
