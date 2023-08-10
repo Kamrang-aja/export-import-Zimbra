3d6ea458568c70a2513bf2427cbf246b8ed59fe2
Kamrang <72611915+Kamrang-aja@users.noreply.github.com>
2023-08-10 21:05:07 +0700

    Update backup_userpass.sh

--- archives/export/userpass/backup_userpass.sh
+++ archives/export/userpass/backup_userpass.sh
@@ -1,4 +1,4 @@
 #=== Scrip find all email account’s passwords in mail server =========================
-for i in `cat /opt/zimbra/backup/archives/export/emails/emails.txt`; 
-	do zmprov  -l ga $i userPassword | grep userPassword: | awk '{ print $2}' > /opt/zimbra/backup/archives/export/userpass/$i.shadow; 
+for i in `cat /archives/export/emails/emails.txt`; 
+	do zmprov  -l ga $i userPassword | grep userPassword: | awk '{ print $2}' > /archives/export/userpass/$i.shadow; 
 done
