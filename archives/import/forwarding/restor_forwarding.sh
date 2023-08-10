8ec65da7c3ff9dbdb598f10ba1c0fd925c7cc03e
Kamrang <72611915+Kamrang-aja@users.noreply.github.com>
2023-08-10 21:07:43 +0700

    Update restor_forwarding.sh

--- archives/import/forwarding/restor_forwarding.sh
+++ archives/import/forwarding/restor_forwarding.sh
@@ -1,6 +1,6 @@
 #=== Scrip restor mails accounts =========================
-for i in `cat /opt/zimbra/backup/archives/import/emails.txt`; do
-    zmprov ma  $i zimbraPrefMailForwardingAddress "`cat /opt/zimbra/backup/archives/import/forwarding/$i.forward`";
+for i in `cat /archives/import/emails.txt`; do
+    zmprov ma  $i zimbraPrefMailForwardingAddress "`cat /archives/import/forwarding/$i.forward`";
     echo "Forward Restore for account ... $i";
 
 done
