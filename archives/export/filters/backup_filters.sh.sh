231c4f6e4f4525c45223432e2e9780d30cd54728
Kamrang <72611915+Kamrang-aja@users.noreply.github.com>
2023-08-10 21:00:07 +0700

    Update backup_filters.sh.sh

--- archives/export/filters/backup_filters.sh.sh
+++ archives/export/filters/backup_filters.sh.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #===== Scrip create filters for emails =========================
-for i in `cat /opt/zimbra/backup/archives/export/emails/emails.txt`; do
+for i in `cat /archives/export/emails/emails.txt`; do
     zmprov ga $i zimbraMailSieveScript > /tmp/filter
     sed -i -e "1d" /tmp/filter
-    sed 's/zimbraMailSieveScript: //g' /tmp/filter  > /opt/zimbra/backup/archives/export/filters/$i.filter
+    sed 's/zimbraMailSieveScript: //g' /tmp/filter  > /archives/export/filters/$i.filter
     rm -f /tmp/filter
     echo "Filter  downloaded for .... $i"
 done 
